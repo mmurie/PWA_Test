5c33507a0bf1643bb79c44169afcaf7e42a2ac09
E190989X <mathieu.murie@etu.univ-nantes.fr>
2022-05-21 09:55:11 +0200

    PWA TEST

---
+++ index.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('load', () => {
+    registerSW();
+});
+
+
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./service-worker.js');
+        } catch (e) {
+            console.log(`SW registration failed`);
+        }
+    } else {
+        console.log("Pas de service worker... :(")
+    }
+}
